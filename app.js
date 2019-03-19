4dc33520df71df7351fabe86bce8564985e95434
wooden1 <iblackheartyou@gmail.com>
2019-03-19 00:34:41 -0400

    added the begining of the quote handling logic

---
+++ app.js
@@ -0,0 +1,7 @@
+// Array of test quotes
+const quotes = [['blah;jv;n', 'gh'],['wow sorry, haha', 'bob']]
+// Iterate over the quotes array and return the quote and author respectively
+const quoteHandler = (arr)=>{arr.forEach((quote) => quote);
+}
+quoteHandler(quotes);
+
